530c97b1e5a7c4328afd14992278d0586f1a6b6a
haider ali <ha347278@gmail.com>
2022-07-03 23:41:03 +0500

    catching intent in second activity

---
+++ app/src/main/java/com/example/foodorderingmultiscreenapp/OrderActivity.java
@@ -0,0 +1,18 @@
+package com.example.foodorderingmultiscreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.MSG);
+    }
+}
