942f5defa5d993f1e1f23c0ee1108da2c226f910
haider ali <ha347278@gmail.com>
2022-07-03 20:35:45 +0500

    java code

--- app/src/main/java/com/example/foodorderingmultiscreenapp/MainActivity.java
+++ app/src/main/java/com/example/foodorderingmultiscreenapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.foodorderingmultiscreenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     }
     public void placeOrder (View view)
     {
+        Intent intent = new Intent(this,OrderActivity.class);
+        EditText editText1 = findViewById(R.id.editText1);
+        EditText editText2 = findViewById(R.id.editText2);
+        EditText editText3 = findViewById(R.id.editText3);
 
     }
 }
